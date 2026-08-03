2c59abbf4cd7a8795e23527ce605a603004da7e8
Davi Barcellos de Lucca <davi@elipse.com.br>
2026-08-03 16:50:22 -0300

    30. Arrays

---
+++ 30Arrays/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <ios>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+
+int main(){
+    
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << std::endl;
+
+    //Way to set collectons in a c++ program
+    //Group variables and manage them as a single entity
+
+    
+    //Declaring and using
+
+
+
+
+
+
+
+    return 0;
+}
